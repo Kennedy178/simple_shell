f77c3b3f8da9c0f9abb1c5ae3151a42e6c9840b6
Kennedy178 <kennedykm178@gmail.com>
2023-04-24 05:26:08 -0700

    added non-interactive mode

--- prompt.c
+++ prompt.c
@@ -6,10 +6,10 @@
 #include <unistd.h>
 
 /**
- * prompt - Simple shell prompt that reads input and executes commands
- * @av: Array of strings representing command-line arguments
- * @env: Array of strings representing environment variables
- */
+* prompt - Simple shell prompt that reads input and executes commands
+* @av: Array of strings representing command-line arguments
+* @env: Array of strings representing environment variables
+*/
 void prompt(char **env)
 {
 char *string = NULL;
@@ -19,23 +19,31 @@ ssize_t ken_size;
 char *argv[2] = {NULL, NULL};
 /* Create child */
 pid_t inherit_pid;
+
+/* Check if running in interactive mode */
+if (isatty(STDIN_FILENO))
+{
 while (1)
 {
 /* Print prompt to ask user for input */
-if (isatty(STDIN_FILENO))
 printf("ken_shell$ ");
+
 /* Read input from user */
 ken_size = getline(&string, &km, stdin);
+
 /* Handle errors */
 if (ken_size == -1)
 {
 free(string);
 exit(EXIT_FAILURE);
 }
+
 /* Remove newline character from input string */
 string[strcspn(string, "\n")] = '\0';
+
 /* Set the command to be executed */
 argv[0] = string;
+
 /* Create child process to execute the command */
 inherit_pid = fork();
 if (inherit_pid == -1)
@@ -44,6 +52,7 @@ free(string);
 perror("fork");
 exit(EXIT_FAILURE);
 }
+
 if (inherit_pid == 0)
 {
 /* Child process: execute the command */
@@ -59,8 +68,52 @@ else
 wait(&state);
 }
 }
+}
+else /* Non-interactive mode*/
+{
+/* Read input from standard input */
+ken_size = getline(&string, &km, stdin);
+
+/* Handle errors */
+if (ken_size == -1)
+{
+free(string);
+exit(EXIT_FAILURE);
+}
+
+/* Remove newline character from input string */
+string[strcspn(string, "\n")] = '\0';
+
+/* Set the command to be executed */
+argv[0] = string;
+
+/* Create child process n execute command */
+inherit_pid = fork();
+if (inherit_pid == -1)
+{
+free(string);
+perror("fork");
+exit(EXIT_FAILURE);
+}
+
+if (inherit_pid == 0)
+{
+/* Child process: execute the command */
+if (execve(argv[0], argv, env) == -1)
+{
+perror("execve");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+/* Parent process: wait for child process to complete*/
+wait(&state);
+}
+
 /* Free memory & exit */
 free(string);
 exit(EXIT_SUCCESS);
+}
 }
 
